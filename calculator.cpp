3891001f1e5ef4832cb52ff7214d1e5ae5bb02a4
GauravBarsker <gauravbarskar4@gmail.com>
2024-08-08 14:03:52 +0530

    basic_calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,56 +1,104 @@
 #include<iostream>
+#include<cmath>
+#include<iomanip>
 using namespace std;
-void sum(long  x,long  y){
-    cout<<x+y;
+//function display for display the menu
+void display(){
+    cout<<"**********************************\n";
+    cout<<"ARITHMETIC OPERATIONS\n";
+    cout<<"1.Addition(+)\n";
+    cout<<"2.Subtraction(-)\n";
+    cout<<"3.Multiplication(*)\n";
+    cout<<"4.Division(/)\n";
+    cout<<"5.Exponentiation(^)\n";
+    cout<<"6.Modulus(%)\n";
+    cout<<"7.Exit\n";
+    cout<<"**********************************\n";
+    cout<<"Select Any Operation(1-7): ";
 }
-void subtraction(int x,int y){
-    cout<<x-y;
+//to add two numbers
+void addition(double num1,double num2){
+    cout<<"\n => "<<num1<<" + "<<num2<<" = "<<(num1+num2)<<"\n\n";
 }
-void multiplication(long x,long y){
-    cout<<x*y;
+//to subtract two numbers
+void subtraction(double num1,double num2){
+    cout<<"\n => "<<num1<<" - "<<num2<<" = "<<(num1-num2)<<"\n\n";
 }
-void division(float x,float y){
-    if (y==0){
-       cout<<"we cant divide any integer by 0";
+//to multiply two numbers
+void multiplication(double num1,double num2){
+    cout<<"\n => "<<num1<<" * "<<num2<<" = "<<(num1*num2)<<"\n\n";
+}
+//to divide two numbers
+void division(double num1,double num2){
+    if (num2==0){
+       cout<<"\n => we cant divide any integer by 0\n\n";
     }
     else {
-        cout<<x/y;
+        cout << "\n => " << num1 << " / " << num2 << " = " << fixed << setprecision(2) << (num1 / num2) << "\n\n";
     }    
 }
-void choice(){
-    cout<<"SELECT ANY OPERATION\n";
-    cout<<"1.Addition\n";
-    cout<<"2.Subtraction\n";
-    cout<<"3.Multiplication\n";
-    cout<<"4.division\n";
+//to find exponentiation
+void exponentiation(int num1,int num2){
+    cout<<"\n => "<<num1<<" ^ "<<num2<<" = "<<pow(num1,num2)<<"\n\n";
+}
+//to find remainder
+void Modulus(double num1,double num2){
+    cout << "\n => " << static_cast<int>(num1) << " % " << static_cast<int>(num2) << " = " << (static_cast<int>(num1) % static_cast<int>(num2)) << "\n\n";
+}
+//to exit from this program
+void exit(){
+    cout<<"\n      =====Exiting the program=====";
 }
 int main (){
-    long long x,y;
-    int a;
-    cout<<"***WELCOME TO SIMPLE CALCULATOR***\n";
-    cout<<"Enter the first number:";
-    cin>>x;
+    double num1,num2;
+    int choice;
+    string repeat;
+    cout<<"       -----WELCOME TO SIMPLE CALCULATOR-----\n\n";
+    do{
+        //display the menu
+    display();
+    cin>>choice;
+       if(choice==7) {
+        exit();
+        break;
+       }    
+    cout<<"\nEnter the first number:";
+    cin>>num1;
     cout<<"Enter the second number:";
-    cin>>y;
-    choice();
-    cout<<"Enter your choice:";
-    cin>>a;
-    switch (a){
+    cin>>num2;
+     switch (choice){
     case 1:
-        sum(x,y);
+        addition(num1,num2);
         break;
     case 2:
-        subtraction(x,y);
+        subtraction(num1,num2);
         break;
     case 3:
-        multiplication(x,y);
+        multiplication(num1,num2);
         break;
     case 4:
-        division(x,y);
-        break;        
+        division(num1,num2);
+        break;
+    case 5:
+        exponentiation(num1,num2);
+        break;
+    case 6:
+        Modulus(num1,num2);
+        break; 
+    case 7:
+        exit();
+        break;                   
     default:
-        cout<<"Invalid choice";
+        cout<<"\nInvalid Choice. Please Select A Valid Operation(1-7)\n\n";
         break;
     }
-    cout<<"\nthank you for use our calculator";
+    cout<<"Do you want of perform another operation?(yes/no): ";
+    cin>>repeat;
+    cout<<"\n";
+
+    }while(repeat=="yes" || repeat=="Yes" || repeat=="YES");
+
+      if(repeat=="no" || repeat=="NO"){
+        cout<<"      =====Exiting the program=====";
+    }  
 }
